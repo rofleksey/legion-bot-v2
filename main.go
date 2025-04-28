433bf05ad91f21f4b08ef65fd90dba06bd125eb2
rofleksey <evilmorty@yandex.ru>
2025-04-29 02:11:12 +0300

    Minor changes

--- main.go
+++ main.go
@@ -69,6 +69,8 @@ var banner string
 func main() {
 	fmt.Fprintln(os.Stderr, banner)
 
+	os.MkdirAll("data", 0750)
+
 	cfg, err := config.Load()
 	if err != nil {
 		log.Fatalf("Config error: %v", err)
