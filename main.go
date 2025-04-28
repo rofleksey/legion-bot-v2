690da940994c22ff3392a551fce1020b7a7df7d4
rofleksey <evilmorty@yandex.ru>
2025-04-29 01:22:32 +0300

    Minor changes

--- main.go
+++ main.go
@@ -29,10 +29,6 @@ import (
 )
 
 // TODO:
-// gallery of killers
-// documentation
-// privacy policy
-// terms
 // greetings
 // ai
 // !spin
@@ -62,6 +58,10 @@ import (
 // steam related checks
 // fmp/dbdtools checks discord
 // played before
+// gallery of killers
+// documentation
+// privacy policy
+// terms
 
 //go:embed banner.txt
 var banner string
@@ -151,13 +151,13 @@ func main() {
 	botInstance := bot.NewBot(database, chatActions, timerManager, localiser, gptInstance, killerMap)
 	botInstance.Init()
 
-	chatProducer := producer.NewTwitchProducer(ircClient, helixClient, database, botInstance)
-	if os.Getenv("ENVIRONMENT") != "production" {
-		chatProducer.AddChannel("tru3ta1ent")
-	}
+	//chatProducer := producer.NewTwitchProducer(ircClient, helixClient, database, botInstance)
+	//if os.Getenv("ENVIRONMENT") != "production" {
+	//	chatProducer.AddChannel("tru3ta1ent")
+	//}
 
-	//chatProducer := producer.NewConsoleProducer(botInstance)
-	//chatProducer.AddChannel("rofleksey")
+	chatProducer := producer.NewConsoleProducer(botInstance)
+	chatProducer.AddChannel("rofleksey")
 
 	sig := make(chan os.Signal, 1)
 	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
